a064e8c82824b3b289966982a40f99faf398b0ce
Johannes Breitling <j.breitling@suited-technologies.com>
2024-02-23 02:42:33 +0100

    Initialized the frontend

---
+++ Mealwhile/src/views/UnitView.vue
@@ -0,0 +1,35 @@
+<template>
+    <main>
+        <v-dialog width="500">
+            <template v-slot:activator="{ props }">
+                <v-btn v-bind="props">Neue Einheit erstellen</v-btn>
+            </template>
+
+            <template v-slot:default="{ isActive }">
+                <v-card title="Neue Einheit erstellen">
+                <v-form>
+                    <v-container>
+                        <v-text-field
+                            label="Name der Einheit (z. B. Gramm)"
+                            hide-details
+                        ></v-text-field>
+                        <v-text-field
+                            label="Abkürzung (z. B. g)"
+                            hide-details
+                        ></v-text-field>
+                        <v-btn>Erstellen</v-btn><br>
+                        <v-btn
+                            text="abbrechen"
+                            @click="isActive.value = false"
+                        ></v-btn>
+                    </v-container>
+                </v-form>
+                </v-card>
+            </template>
+        </v-dialog>        
+    </main>
+</template>
+
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
